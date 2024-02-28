720e5c4053e0723b2c93da8a83f0f674f05a2ec6
Sanvals <santano.sergio@gmail.com>
2024-02-28 14:12:56 +0100

    Helpers and Tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likesList = blogs.map(b => b.likes)
+    const total = likesList.reduce((acc, currentValue) => acc + currentValue, 0)
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
