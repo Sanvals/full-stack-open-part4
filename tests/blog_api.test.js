f002d74e3656ccf72af851437758faadccdb3b1c
Sanvals <santano.sergio@gmail.com>
2024-02-28 18:42:04 +0100

    Blog List Tests, step 3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const assert = require('assert')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
@@ -17,11 +18,36 @@ test('there are two blog posts', async () => {
     assert.strictEqual(response.body.length, 2)
 })
 
-test.only('id is the primary identifier property', async () => {
+test('id is the primary identifier property', async () => {
     const response = await api.get('/api/blogs')
     response.body.forEach(b => {
         assert.strictEqual("id" in b, true)
     })
+})
+
+test.only('creates new blog via POST and checks the count', async () => {
+    const prevCounter = await api.get('/api/blogs')
+    const likesGenerator = Math.round(Math.random() * 100)
+    
+    const newBlog =
+        {
+            "title": "The Things 3",
+            "author": "Arto",
+            "url": "http://www.google.es",
+            "likes": likesGenerator
+        }
+
+    await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const afterCounter = await api.get('/api/blogs')
+    
+    // Check that the counter increases in one
+    assert.strictEqual(prevCounter.body.length, afterCounter.body.length -1)
+    // Check that the created item has the correct values
+    assert.strictEqual(afterCounter.body.slice(-1)[0].likes, likesGenerator)
 })
 
 after(async () => {
